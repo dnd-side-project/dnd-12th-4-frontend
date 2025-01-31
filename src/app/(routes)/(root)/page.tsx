b21c6c6a74f257e6805f01bced3bf5d333c0048c
hyunjun9788 <namhyunjun9788@gmail.com>
2025-01-31 14:43:44 +0900

    feat: 타이틀 및 이미지 구현 #3

--- src/app/(routes)/(root)/page.tsx
+++ src/app/(routes)/(root)/page.tsx
@@ -1,9 +1,21 @@
 import HeaderFooterWrapper from "@/components/layout/HeaderFooterWrapper"
 
+import Image from "next/image"
+
 export default function Home() {
   return (
     <HeaderFooterWrapper header footer>
-      <p className="">테스트 페이지</p>
+      <div className="mb-6 mt-[58px] flex justify-center">
+        <section className="flex w-full flex-col gap-[57px] px-4">
+          <div className="flex flex-col items-center gap-10">
+            <div className="text-center text-2xl font-semibold">
+              친구와 무전을 주고 받으며
+              <br />더 알아갈 수 있어요.
+            </div>
+            <Image src="https://placehold.co/229x229.png" width={229} height={229} alt="일러스트 이미지" />
+          </div>
+        </section>
+      </div>
     </HeaderFooterWrapper>
   )
 }
