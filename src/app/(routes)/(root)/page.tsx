390be6c1d515a3a0670c071eae03be33833d708f
KoJaem <woalswhwh@naver.com>
2025-02-03 04:58:38 +0900

    feat: TitleSection 구현 #5

--- src/app/(routes)/(root)/page.tsx
+++ src/app/(routes)/(root)/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import HeaderFooterWrapper from "@/components/layout/HeaderFooterWrapper"
+import TitleSection from "@/components/root/TitleSection"
 
 export default function Home() {
   return (
-    <HeaderFooterWrapper header footer>
-      <p className="">테스트 페이지</p>
+    <HeaderFooterWrapper footer>
+      <section className="flex h-full flex-col bg-[#292D30]">
+        <TitleSection />
+      </section>
     </HeaderFooterWrapper>
   )
 }
