784489bff604e9e1afa1315ec36c8de3498fcfd5
KoJaem <woalswhwh@naver.com>
2025-02-03 06:10:16 +0900

    feat: 🚀 Swiper UI 개선 (부모 padding 무시) #5

--- src/components/root/FriendsSlider.tsx
+++ src/components/root/FriendsSlider.tsx
@@ -1,22 +1,24 @@
 import TitleWithMoreView from "@/components/root/TitleWithMoreView"
 import Image from "next/image"
-import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
+import { Swiper, SwiperSlide } from "swiper/react"
 
 export default function FriendsSlider() {
   return (
     <article className="flex flex-col gap-[12px]">
       <TitleWithMoreView title={"참여 중인 친구 14명"} onClick={() => {}} />
-      <Swiper slidesPerView={3} spaceBetween={8} className="w-full">
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((data) => (
-          <SwiperSlide key={data} className="w-fit rounded-[12px] bg-white p-[20px]">
-            <div className="flex flex-col items-center gap-[8px]">
-              <Image src={"/favicon.ico"} width={48} height={48} alt="profile-image" />
-              <p className="text-[14px] font-semibold text-black/60">CODE000</p>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="mx-[-16px]">
+        <Swiper slidesPerView="auto" spaceBetween={8} className="w-full" slidesOffsetAfter={16} slidesOffsetBefore={16}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((data) => (
+            <SwiperSlide key={data} className="!w-fit rounded-[12px] bg-white">
+              <div className="flex w-[106px] max-w-[50vw] flex-col items-center gap-[8px] p-[20px]">
+                <Image src={"/favicon.ico"} width={48} height={48} alt="profile-image" />
+                <p className="text-[14px] font-semibold text-black/60">CODE000</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </article>
   )
 }
--- src/components/root/TodayAnswerSlider.tsx
+++ src/components/root/TodayAnswerSlider.tsx
@@ -1,3 +1,4 @@
+import TitleWithMoreView from "@/components/root/TitleWithMoreView"
 import "@/styles/todayAnswerSlider.css"
 import { MessageCircle } from "lucide-react"
 import Image from "next/image"
@@ -5,45 +6,50 @@ import "swiper/css"
 import "swiper/css/pagination"
 import { Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
-import TitleWithMoreView from "@/components/root/TitleWithMoreView"
 
 export default function TodayAnswerSlider() {
   return (
     <article className="flex flex-col gap-[12px]">
       <TitleWithMoreView title={"오늘 응답 5개"} onClick={() => {}} />
-      <Swiper
-        className="relative h-fit w-full"
-        modules={[Pagination]}
-        pagination={{
-          el: `#today-answer-slider`,
-          clickable: true,
-          renderBullet: function (index, className) {
-            return `<span class="${className} !bg-black !w-[4px] !h-[4px]"> </span>`
-          }
-        }}
-      >
-        {[0, 1, 2].map((data) => (
-          <SwiperSlide key={data}>
-            <article className="flex flex-col gap-[12px] rounded-[20px] bg-white p-[20px]">
-              <div className="flex items-center gap-[8px]">
-                <Image src={"/favicon.ico"} width={32} height={32} alt="profile-image" />
-                <div className="flex flex-col text-black/60">
-                  <p className="text-[14px]">CODE000</p>
-                  <p className="text-[12px]">1시간전</p>
+      <div className="mx-[-16px]">
+        <Swiper
+          className="relative h-fit w-full"
+          slidesPerView="auto"
+          modules={[Pagination]}
+          slidesOffsetBefore={16}
+          slidesOffsetAfter={16}
+          spaceBetween={8}
+          pagination={{
+            el: `#today-answer-slider`,
+            clickable: true,
+            renderBullet: function (index, className) {
+              return `<span class="${className} !bg-black !w-[4px] !h-[4px]"> </span>`
+            }
+          }}
+        >
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((data) => (
+            <SwiperSlide key={data} className="!w-fit">
+              <article className="flex w-[360px] max-w-[90vw] flex-col gap-[12px] rounded-[20px] bg-white p-[20px]">
+                <div className="flex items-center gap-[8px]">
+                  <Image src={"/favicon.ico"} width={32} height={32} alt="profile-image" />
+                  <div className="flex flex-col text-black/60">
+                    <p className="text-[14px]">CODE000</p>
+                    <p className="text-[12px]">1시간전</p>
+                  </div>
                 </div>
-              </div>
-              <p className="line-clamp-2 text-[14px]">
-                이번 주 7시에 다 같이 모여서 저녁 먹자 이번 주 7시에 다 같이 모여서 저녁 먹자이번 주 7시에 다 같이
-                모여서 저녁 먹자이번 주 7시에 다 같이 모여서 저녁 먹자이번 주 7시에 다 같이 모여서 저녁 먹자
-              </p>
-              <div className="flex items-center gap-[4px]">
-                <MessageCircle size={16} color="#00000099" />
-                <p className="text-black/60">000</p>
-              </div>
-            </article>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+                <p className="line-clamp-2 text-[14px]">
+                  이번 주 7시에 다 같이 모여서 저녁 먹자 이번 주 7시에 다 같이 모여서 저녁 먹자이번 주 7시에 다 같이
+                  모여서 저녁 먹자이번 주 7시에 다 같이 모여서 저녁 먹자이번 주 7시에 다 같이 모여서 저녁 먹자
+                </p>
+                <div className="flex items-center gap-[4px]">
+                  <MessageCircle size={16} color="#00000099" />
+                  <p className="text-black/60">000</p>
+                </div>
+              </article>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
       <section id="today-answer-slider" className="flex w-full justify-center gap-[4px]" />
     </article>
   )
