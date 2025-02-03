e4e80b598a0ee98bc432bc28d1e414f6ad631144
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-03 23:18:45 +0900

    chore: 구글 로그인 버튼 삭제 #3

--- src/components/auth/auth-button/index.tsx
+++ src/components/auth/auth-button/index.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { GoogleSymbol } from "@/icons/GoogleSymbol"
 import { KakaoSymbol } from "@/icons/KakaoSymbol"
 import { signIn } from "next-auth/react"
 import React from "react"
@@ -7,10 +6,6 @@ import React from "react"
 export default function AuthButton() {
   return (
     <div className="flex w-full flex-col gap-3">
-      <button className="flex items-center justify-center gap-3 rounded-xl border border-[#9CAAB9] py-4 hover:opacity-70 active:opacity-80">
-        <GoogleSymbol />
-        Google로 시작하기
-      </button>
       <button
         className="flex items-center justify-center gap-3 rounded-xl bg-[#FEE500] py-4 hover:opacity-70 active:opacity-80"
         onClick={() => signIn("kakao")}
