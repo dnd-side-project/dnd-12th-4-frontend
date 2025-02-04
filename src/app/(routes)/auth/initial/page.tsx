dd6c7ebb344dbe3c770c7b2a1ee8f93ab0816cd3
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-05 01:08:23 +0900

    feat: initial 페이지 구현 #7

---
+++ src/app/(routes)/auth/initial/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Guide from "@/components/auth/Guide"
+import { FIRST_PAGE, GUIDE_INFO } from "@/constants/auth"
+import { useState } from "react"
+
+function Page() {
+  const [page, setPage] = useState(FIRST_PAGE)
+  const onNext = () => {
+    setPage((prev) => prev + 1)
+  }
+  return <Guide guideInfo={GUIDE_INFO.CHARACTER_INTRODUCE[page]} onNext={onNext} isButton />
+}
+
+export default Page
