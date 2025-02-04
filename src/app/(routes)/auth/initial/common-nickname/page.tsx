f2cef66be5dbe0fb17910fed2a402233b7fa674f
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-05 01:10:31 +0900

    feat: 공용 닉네임 작성 페이지 구현 #7

---
+++ src/app/(routes)/auth/initial/common-nickname/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Guide from "@/components/auth/Guide"
+import InitialSetupForm from "@/components/auth/initial-setup-form"
+import { FIRST_PAGE, GUIDE_INFO, INITIAL_SETUP_INFO, SECOND_PAGE } from "@/constants/auth"
+import React, { useState } from "react"
+
+function Page() {
+  const [page, setPage] = useState(FIRST_PAGE)
+  const [nickname, setNickname] = useState("")
+  // const { url, title, description, isLastPage } = INITIAL_SETUP_INFO.COMMON_NICKNAME
+  const onNext = () => {
+    setPage((prev) => prev + 1)
+  }
+
+  return (
+    <div>
+      {page === FIRST_PAGE && (
+        <InitialSetupForm
+          initialSetupInfo={INITIAL_SETUP_INFO.COMMON_NICKNAME}
+          onNext={onNext}
+          setInput={setNickname}
+        />
+      )}
+      {page === SECOND_PAGE && <Guide guideInfo={GUIDE_INFO.COMMON_NICKNAME} nickname={nickname} isButton />}
+    </div>
+  )
+}
+
+export default Page
