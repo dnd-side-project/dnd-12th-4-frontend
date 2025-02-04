8bbd7e0a05f680cc29b724c25b47618581422741
KoJaem <woalswhwh@naver.com>
2025-02-04 13:31:20 +0900

    chore: question validation schema 수정

--- src/validations/questionSchema.ts
+++ src/validations/questionSchema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 
 export const questionSchema = z.object({
-  question: z.string().max(100, "질문은 100자 이내로 작성해주세요.").nonempty("질문은 필수입니다.")
+  question: z.string().max(100, "질문은 100자 이내로 작성해주세요.").nonempty("질문은 필수입니다."),
+  isAnonymous: z.boolean()
 })
