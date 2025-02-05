0606b7a17e4f77c080193143f374554280a34923
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-05 22:44:03 +0900

    feat: 초대 코드 알려주기 퍼널 구현 #7

---
+++ src/app/(routes)/auth/initial/create-or-notify/notify-code/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Guide from "@/components/auth/guide"
+import InitialSetupForm from "@/components/auth/initial-setup-form"
+import { GUIDE_INFO, INITIAL_SETUP_INFO } from "@/constants/auth"
+import { useState } from "react"
+
+const steps = ["InvitationCode", "ChannelNickname", "CreatedCode"]
+
+function Page() {
+  const [stepLevel, setStepLevel] = useState(0)
+  const onNext = () => {
+    setStepLevel((prev) => prev + 1)
+  }
+  return (
+    <div>
+      {steps[stepLevel] === "InvitationCode" && (
+        <InitialSetupForm initialSetupInfo={INITIAL_SETUP_INFO.INVITATION_CODE} onNext={onNext} showButton />
+      )}
+      {steps[stepLevel] === "ChannelNickname" && (
+        <InitialSetupForm initialSetupInfo={INITIAL_SETUP_INFO.INVITE_CHANNEL_NICKNAME} onNext={onNext} showButton />
+      )}
+      {steps[stepLevel] === "CreatedCode" && (
+        <Guide guideInfo={GUIDE_INFO.CHANNEL_NICKNAME} onNext={onNext} isButton={false} />
+      )}
+    </div>
+  )
+}
+
+export default Page
