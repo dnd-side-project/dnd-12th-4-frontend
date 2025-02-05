067ca45c4ee2f531e75fc1cc39320773f54a45dc
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-05 14:03:54 +0900

    feat: 로그인 성공 및 신규 유저 시 auth/initial 이동 #7

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -1,16 +1,21 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth"
 import AuthButton from "@/components/auth/auth-button"
 import CarouselItems from "@/components/auth/carousel-items"
-import HeaderFooterWrapper from "@/components/layout/HeaderFooterWrapper"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
 
-export default function Auth() {
+export default async function Auth() {
+  const session = await getServerSession(authOptions)
+
+  if (session && session.user.isNewMember) {
+    redirect("/auth/initial/")
+  }
   return (
-    <HeaderFooterWrapper header footer>
-      <div className="mb-6 mt-[58px] flex justify-center">
-        <section className="flex w-full flex-col items-center gap-[57px] px-4">
-          <CarouselItems />
-          <AuthButton />
-        </section>
-      </div>
-    </HeaderFooterWrapper>
+    <div className="mb-6 mt-[58px] flex justify-center">
+      <section className="flex w-full flex-col items-center gap-[57px] px-4">
+        <CarouselItems />
+        <AuthButton />
+      </section>
+    </div>
   )
 }
