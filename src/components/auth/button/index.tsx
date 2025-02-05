5e5d98cea7444f3fc8c21a0f91d2ce6582e81dc8
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-05 22:07:26 +0900

    refactor: 버튼 공용 컴포넌트 리팩토링 #7

---
+++ src/components/auth/button/index.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { cn } from "@/utils/cn"
+import React from "react"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: keyof typeof buttonVariants
+  size: keyof typeof buttonSizes
+  children: React.ReactNode
+  className?: string
+}
+
+const buttonVariants = {
+  default: "cursor-pointer rounded-xl font-medium bg-[#FDD835]",
+  kakao: "relative flex items-center justify-center rounded-xl bg-[#FEE500] hover:opacity-70 active:opacity-80"
+}
+
+const buttonSizes = {
+  default: "h-[54px] w-full",
+  kakao: "h-[51.45px] w-full"
+}
+
+export default function Button({ variant, children, size, className, ...props }: ButtonProps) {
+  return (
+    <button className={cn(buttonVariants[variant], buttonSizes[size], className)} {...props}>
+      {children}
+    </button>
+  )
+}
