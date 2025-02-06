2d5cd3abc53b8a58fe7e2ca61180a9fca05faa47
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-06 16:24:28 +0900

    chore: 스타일 규칙 반영

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -13,7 +13,7 @@ export default async function Auth() {
 
   return (
     <div className="flex h-full justify-center pb-[12px] pt-[56px]">
-      <section className="flex w-full flex-col items-center justify-between gap-[57px] px-4">
+      <section className="flex w-full flex-col items-center justify-between gap-[57px] px-[16px]">
         <CarouselItems />
         <LoginButton />
       </section>
