8b5d5180880849fdd3a3624d06f0cf5b3ebf5ce6
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-07 12:31:54 +0900

    feat: 탭 메뉴별로 탭 메뉴 및 정렬 박스 구현 #14

---
+++ src/app/(routes)/my-page/channel-profile/my-channel/page.tsx
@@ -0,0 +1,9 @@
+import CountAndSortBox from "@/components/my-page/CountAndSortBox"
+
+export default function MyChannelPage() {
+  return (
+    <div>
+      <CountAndSortBox type="채널" count={333} />
+    </div>
+  )
+}
--- src/app/(routes)/my-page/channel-profile/page.tsx
+++ src/app/(routes)/my-page/channel-profile/page.tsx
@@ -1,20 +1,15 @@
-import HeaderFooterWrapper from "@/components/layout/HeaderFooterWrapper"
 import { ChannelCard } from "@/components/my-page/ChannelCard"
 import CountAndSortBox from "@/components/my-page/CountAndSortBox"
-import { TabList } from "@/components/my-page/TabList"
 
 export default async function ChannelProfilePage() {
   return (
-    <HeaderFooterWrapper header footer headerTitle="채널 프로필">
-      <div className="flex flex-col px-[16px] py-[12px]">
-        <TabList />
-        <CountAndSortBox type="채널" count={333} />
-        <ChannelCard
-          imageSrc="https://placehold.co/229x229.png"
-          nickname={"닉네임이에요"}
-          channelName={"채널명: 채널명23123123"}
-        />
-      </div>
-    </HeaderFooterWrapper>
+    <div className="flex flex-col">
+      <CountAndSortBox type="채널" count={333} />
+      <ChannelCard
+        imageSrc="https://placehold.co/229x229.png"
+        nickname={"닉네임이에요"}
+        channelName={"채널명: 채널명23123123"}
+      />
+    </div>
   )
 }
