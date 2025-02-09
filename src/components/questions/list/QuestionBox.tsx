dc50a6768213aac40ebb9f26e58ea8d8ea459140
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-10 00:04:42 +0900

    feat: 질문 박스 구현 #20

---
+++ src/components/questions/list/QuestionBox.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { MessageCircle } from "lucide-react"
+import { useRouter, useSearchParams } from "next/navigation"
+
+interface QuestionBoxProps {
+  count: number
+  nickname: string
+  time: number
+  text: string
+}
+export default function QuestionBox({ count, nickname, time, text }: QuestionBoxProps) {
+  const searchParams = useSearchParams()
+  const tab = searchParams.get("tab")
+  const router = useRouter()
+
+  const handleBoxClick = () => {
+    router.push(`questions/detail/${2}`)
+  }
+
+  return (
+    <section
+      onClick={handleBoxClick}
+      className="flex cursor-pointer flex-col gap-[12px] rounded-[24px] bg-[#F5F8FA] px-[20px] py-[24px]"
+    >
+      <p className="inline-block w-fit rounded-[40px] bg-[#D7DFE7] px-[12px] py-[4px] text-[12px] font-medium text-black/60">
+        {count}번째 시그널
+      </p>
+      <div className="text-[14px] font-medium">{text}</div>
+      <div className="flex text-[12px]">
+        <div className="flex flex-1 gap-[12px]">
+          {tab !== "my-signal" && <p className="font-medium">{nickname}</p>}
+          <p className="text-black/60">{time}시간 전</p>
+        </div>
+        <div className="flex items-center gap-[4px]">
+          <MessageCircle className="size-[16px]" />
+          <p className="text-black/60">{count}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/questions/list/QuestionListTabBox.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import Tab from "@/components/common/Tab"
+import { TAB_MENUS } from "@/constants/tab"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+
+export default function QuestionListTabBox() {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const pathname = usePathname()
+  const tab = searchParams.get("tab")
+  return (
+    <ul className="flex gap-[8px]">
+      {TAB_MENUS.QUESTION.map((TAB_MENU) => (
+        <Tab
+          key={TAB_MENU.value}
+          label={TAB_MENU.label}
+          isActive={tab === TAB_MENU.value}
+          onClick={() => router.push(`${pathname}?tab=${TAB_MENU.value}`)}
+        />
+      ))}
+    </ul>
+  )
+}
