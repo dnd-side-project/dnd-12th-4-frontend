57ed900197be7674a9874b42d6d47acb0d597636
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-13 21:06:00 +0900

    fix: middleware 버그 수정 #26

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,20 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
-import { PROTECTED_ROUTES } from "@/constants/app-routes"
 
 export default async function middleware(request: NextRequest) {
   const token = await getToken({ req: request })
   const { pathname } = request.nextUrl
-  const isNumericPath = pathname.match(/^\/\d+($|\/.*)/)
 
-  if ((!token && PROTECTED_ROUTES.some((route) => pathname.startsWith(route))) || isNumericPath) {
+  if (pathname === "/auth" || pathname === "/") {
+    return NextResponse.next()
+  }
+
+  if (!token) {
     return NextResponse.redirect(new URL("/auth", request.url))
   }
+
   return NextResponse.next()
 }
 export const config = {
-  matcher: ["/channels/:path+", "/auth/initial/:path*", "/my-page/:path*", "/(\\d+)(.*)?"]
+  matcher: ["/((?!^$|^auth$|api|_next|static|public|favicon.ico|.+\\.[\\w]+$).*)"]
 }
