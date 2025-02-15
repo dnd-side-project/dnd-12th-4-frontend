3357c89cc911c9a52dd881e6edc9569981cadb92
KoJaem <woalswhwh@naver.com>
2025-02-16 02:31:27 +0900

    deploy: 빌드 테스트

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 cd ../
 mkdir output
+
 cp -R ./dnd-12th-4-frontend/* ./output
+
+cp ./dnd-12th-4-frontend/.gitignore ./output
+cp ./dnd-12th-4-frontend/.prettierrc.json ./output
+
 cp -R ./output ./dnd-12th-4-frontend/
