13d4a985712500c22563b29f260179e71df0d44f
KoJaem <woalswhwh@naver.com>
2025-02-15 14:07:15 +0900

    chore: 바텀시트 min-h 설정 및 디자인시스템 적용 #6

--- src/components/questions/new/RecommendQuestionBottomSheet.tsx
+++ src/components/questions/new/RecommendQuestionBottomSheet.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Button from "@/components/common/Button"
 import { category } from "@/dummy/categoryData"
+import { XIcon } from "lucide-react"
 import { AnimatePresence, motion } from "motion/react"
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react"
 import { Sheet } from "react-modal-sheet"
@@ -37,9 +38,12 @@ export default function RecommendQuestionBottomSheet({ isOpen, setIsOpen }: Para
       <Sheet.Container>
         <Sheet.Content>
           <Sheet.Scroller>
-            <section className="flex h-full flex-col justify-between overflow-hidden px-[20px] pb-[80px] pt-[16px] text-black">
+            <section className="flex h-full min-h-[400px] flex-col justify-between overflow-hidden px-[20px] pb-[80px] pt-[16px] text-black">
               <article className="flex flex-col items-center">
-                <p className="text-[20px] font-semibold">추천 시그널</p>
+                <article className="flex w-full justify-between">
+                  <p className="text-subtitle-02 font-semibold text-emphasis-high">추천 시그널</p>
+                  <XIcon size={20} color="#000000DE" />
+                </article>
                 <article className="mt-[8px] flex w-full flex-col">
                   <div className="relative flex whitespace-nowrap">
                     {Object.keys(category).map((objectKey, index) => (
@@ -48,7 +52,7 @@ export default function RecommendQuestionBottomSheet({ isOpen, setIsOpen }: Para
                         ref={(el) => {
                           buttonsRef.current[index] = el
                         }}
-                        className="flex-1 bg-white"
+                        className="flex-1 bg-white text-body-03"
                         onClick={() => {
                           setSelectedKey(objectKey as keyof typeof category)
                           setSelectedIndex(index)
@@ -57,10 +61,10 @@ export default function RecommendQuestionBottomSheet({ isOpen, setIsOpen }: Para
                         {category[objectKey as keyof typeof category].label}
                       </Button>
                     ))}
-                    <div className="absolute bottom-0 left-0 right-0 h-[4px] rounded-full bg-black/20">
+                    <div className="absolute bottom-0 left-0 right-0 h-[4px] rounded-full bg-outline">
                       <AnimatePresence>
                         <motion.span
-                          className="absolute bottom-0 h-[4px] rounded-full bg-black"
+                          className="absolute bottom-0 h-[4px] rounded-full bg-gray-08"
                           initial={{ left: indicatorLeft, width: indicatorWidth }}
                           animate={{ left: indicatorLeft, width: indicatorWidth }}
                           transition={{ duration: 0.3 }}
@@ -69,11 +73,15 @@ export default function RecommendQuestionBottomSheet({ isOpen, setIsOpen }: Para
                     </div>
                   </div>
                   <article className="flex w-full flex-col gap-[8px] py-[20px]">
-                    {category[selectedKey]?.items.map((data, i) => <p key={i}>{data}</p>)}
+                    {category[selectedKey]?.items.map((data, i) => (
+                      <p className="text-body-03 text-emphasis-high" key={i}>
+                        {data}
+                      </p>
+                    ))}
                   </article>
                 </article>
               </article>
-              <article className="absolute bottom-0 left-[0px] right-[0px] h-[80px] w-full content-center bg-white px-[16px]">
+              <article className="absolute bottom-0 left-[0px] right-[0px] h-[80px] w-full content-center bg-white px-[16px] text-body-01 text-emphasis-high">
                 <Button className="w-full" type="button" onClick={() => setIsOpen(false)}>
                   확인
                 </Button>
