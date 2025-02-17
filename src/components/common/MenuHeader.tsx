ddbdb13381abe3fa662b6c1486d7b251f65e9fd2
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-17 20:28:44 +0900

    chore: MenuHeader 스타일 수정 #41

--- src/components/common/MenuHeader.tsx
+++ src/components/common/MenuHeader.tsx
@@ -1,18 +1,33 @@
 "use client"
 
+import { cn } from "@/utils/cn"
+
 interface MenuHeaderProps {
   title: string
   button?: boolean
   onClick?: () => void
   buttonTitle?: string
+  buttonClassName?: string
+  titleClassName?: string
 }
 
-export default function MenuHeader({ title, button, onClick, buttonTitle }: MenuHeaderProps) {
+export default function MenuHeader({
+  title,
+  button,
+  onClick,
+  buttonTitle,
+  buttonClassName,
+  titleClassName
+}: MenuHeaderProps) {
   return (
     <div className="flex h-[56px] items-center justify-between py-[12px]">
-      <div className="text-headline-02 font-bold">{title}</div>
+      <div className={cn("text-headline-02 font-bold", titleClassName)}>{title}</div>
       {button && (
-        <button className="rounded-[8px] bg-gray-03 px-[12px] py-[4px] text-caption-01" type="button" onClick={onClick}>
+        <button
+          className={cn("rounded-[8px] bg-gray-03 px-[12px] py-[4px] text-caption-01", buttonClassName)}
+          type="button"
+          onClick={onClick}
+        >
           {buttonTitle}
         </button>
       )}
