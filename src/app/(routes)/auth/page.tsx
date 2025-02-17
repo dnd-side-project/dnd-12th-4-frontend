de4c986d92c3cffb5849afc86dee0b80542c3d4e
KoJaem <woalswhwh@naver.com>
2025-02-18 05:29:34 +0900

    feat: splash Swiper 구현 #45

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -25,7 +25,7 @@ export default async function Auth() {
 
   return (
     <div className="flex h-full justify-center pb-[12px] pt-[56px]">
-      <section className="flex w-full flex-col items-center justify-between gap-[57px] px-[16px]">
+      <section className="flex w-full flex-col items-center justify-between px-[16px]">
         <CarouselItems />
         <LoginButton />
       </section>
