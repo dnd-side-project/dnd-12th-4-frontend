c433340ef6b0c7cb336041cbbb71ce309aa18a5b
KoJaem <woalswhwh@naver.com>
2025-02-17 14:24:27 +0900

    chore: 백엔드 테스트용 console.log 추가

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -1,7 +1,11 @@
+"use client"
 import LoginButton from "@/components/auth/LoginButton"
 import CarouselItems from "@/components/auth/CarouselItems"
+import { useSession } from "next-auth/react"
 
-export default async function Auth() {
+export default function Auth() {
+  const { data: session } = useSession()
+  console.log("액세스토큰", session?.user?.accessToken)
   return (
     <div className="flex h-full justify-center pb-[12px] pt-[56px]">
       <section className="flex w-full flex-col items-center justify-between gap-[57px] px-[16px]">
