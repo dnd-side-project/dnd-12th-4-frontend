4f04cdde412bb93dff3c7fc92dfcc3441601d215
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-18 20:19:45 +0900

    chore: 카운트 및 정렬 박스 스타일 수정 #46

--- src/components/my-page/CountAndSortBox.tsx
+++ src/components/my-page/CountAndSortBox.tsx
@@ -3,24 +3,26 @@ import { ChevronDown } from "lucide-react"
 import FilterBottomSheet from "@/components/channels/FilterBottomSheet"
 import { useState } from "react"
 import { useSearchParams } from "next/navigation"
+import { cn } from "@/utils/cn"
 
 interface CountAndSortBoxProps {
   type: "채널" | "응답" | "시그널"
   count: number
+  className?: string
 }
-export default function CountAndSortBox({ type, count }: CountAndSortBoxProps) {
+export default function CountAndSortBox({ type, count, className }: CountAndSortBoxProps) {
   const searchParams = useSearchParams()
   const sort = searchParams.get("sort")
   const [isOpenFilterSheet, setIsOpenFilterSheet] = useState(false)
 
   return (
     <div className="flex justify-between py-[12px]">
-      <div className="flex items-center gap-[2px] text-body-03 text-emphasis-high">
+      <div className={cn("flex items-center gap-[4px] text-body-03 text-emphasis-high", className)}>
         <div>{type}</div>
         <div className="text-secondary-300">{count}</div>
       </div>
       <button className="flex items-center gap-[2px]" onClick={() => setIsOpenFilterSheet(true)}>
-        <div className="text-[14px] font-medium">{sort === "oldest" ? "오래된 순" : "최신순"}</div>
+        <div className="text-body-03 text-emphasis-medium">{sort === "oldest" ? "오래된 순" : "최신순"}</div>
         <ChevronDown size={12} />
       </button>
       <FilterBottomSheet isOpen={isOpenFilterSheet} setIsOpen={setIsOpenFilterSheet} />
