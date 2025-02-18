bcee7883b645bedc48c297bcff9dab24f09a5466
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-18 15:29:53 +0900

    feat: 추가 버튼 클릭 시 바텀 시트 생성

---
+++ src/components/my-page/MyPageClient.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React, { useState } from "react"
+import MenuHeader from "../common/MenuHeader"
+import FeedbackBox from "./my-info/FeedbackBox"
+import ChannelSection from "./my-info/ChannelSection"
+import ProfileCard from "./profile/ProfileCard"
+import ChannelBottomSheet from "../channels/ChannelBottomSheet"
+
+export default function MyPageClient() {
+  const [isOpenChannelSheet, setIsOpenChannelSheet] = useState(false)
+  return (
+    <>
+      <div className="px-[16px]">
+        <MenuHeader title="내 정보" />
+        <div className="flex flex-col gap-[20px]">
+          <ProfileCard />
+          <FeedbackBox />
+          <div>
+            <MenuHeader
+              title="현재 속한 채널"
+              buttonTitle="추가"
+              button
+              buttonClassName="bg-gray-08 text-caption-01 text-white"
+              titleClassName="text-subtitle-01 font-semibold"
+              onClick={() => setIsOpenChannelSheet(true)}
+            />
+            <ChannelSection />
+          </div>
+        </div>
+      </div>
+      <ChannelBottomSheet isOpen={isOpenChannelSheet} setIsOpen={setIsOpenChannelSheet} />
+    </>
+  )
+}
