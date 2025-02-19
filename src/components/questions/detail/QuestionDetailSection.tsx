f378437ce3a5bc81192f1f437048eafaa0002d61
hyunjun9788 <namhyunjun9788@gmail.com>
2025-02-19 23:04:40 +0900

    feat: 질문 상세 박스 api 연동

--- src/components/questions/detail/QuestionDetailSection.tsx
+++ src/components/questions/detail/QuestionDetailSection.tsx
@@ -2,20 +2,35 @@
 
 import QuestionOrAnswerDetailBox from "@/components/questions/detail/QuestionOrAnswerDetailBox"
 import MyAnswerStatusBox from "@/components/questions/detail/MyAnswerStatusBox"
+import { useFindQuestionsByQuestionId } from "@/api/question-controller/question-controller"
+import { useParams } from "next/navigation"
 
 export default function QuestionDetailSection() {
+  const params = useParams()
+  const channelId = String(params.id)
+  const questionId = Number(params.questionId)
   const isMyAnswer = false
+  const { data } = useFindQuestionsByQuestionId(channelId, questionId)
   return (
-    <section className="flex flex-col gap-[12px]">
-      <QuestionOrAnswerDetailBox
-        type="question"
-        imageSrc="https://placehold.co/230x230.png"
-        content="가이드 텍스트입니다.가이드 텍스트입니다.가이드 텍스트입니다.가이드 텍스트입니다.가이드 텍스트입니다.가이드
-              텍스트입니다.가이드 텍스트입니다.가이드 텍스트입니다.가이드 텍스트입니다.가"
-        nickname="닉네임"
-        time={3}
-      />
-      <MyAnswerStatusBox title={isMyAnswer ? "응답을 보내셨네요. 정말 멋져요!" : "질문자가 응답을 기다리고 있어요!"} />
-    </section>
+    <>
+      <div className="text-subtitle-01">
+        <span className="text-secondary-300">{data?.signalNumber}번째 </span>
+        <span className="text-emphasis-high">시그널</span>
+      </div>
+      <section className="flex flex-col gap-[12px]">
+        <QuestionOrAnswerDetailBox
+          type="question"
+          channelId={channelId}
+          questionId={questionId}
+          imageSrc="https://placehold.co/230x230.png"
+          content={data?.content || ""}
+          nickname={data?.writerName || ""}
+          time={data?.createdAt || ""}
+        />
+        <MyAnswerStatusBox
+          title={isMyAnswer ? "응답을 보내셨네요. 정말 멋져요!" : "질문자가 응답을 기다리고 있어요!"}
+        />
+      </section>
+    </>
   )
 }
