715102a90c04b1f4a2c7b39a5e59ee4743c3f86e
KoJaem <woalswhwh@naver.com>
2025-02-20 07:05:32 +0900

    chore: InviteMission 컴포넌트 태그 삭제

--- src/components/root/InviteMission.tsx
+++ src/components/root/InviteMission.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-import Image from "next/image"
-import Tag from "@/components/common/Tag"
 import { useFindChannelById } from "@/api/channel-controller/channel-controller"
-import { useState } from "react"
-import { useParams } from "next/navigation"
 import InviteBottomSheet from "@/components/root/InviteBottomSheet"
+import Image from "next/image"
+import { useParams } from "next/navigation"
+import { useState } from "react"
 
 export default function InviteMission() {
   const { id } = useParams()
@@ -22,7 +21,7 @@ export default function InviteMission() {
     >
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-[12px]">
-          <Tag text={`1번째 시그널`} className="bg-gray-01 text-emphasis-high" />
+          {/* <Tag text={`1번째 시그널`} className="bg-gray-01 text-emphasis-high" /> */}
           <div className="flex flex-col gap-[4px]">
             <p className="text-subtitle-01 font-semibold text-emphasis-high">친구들을 초대해보세요!</p>
             <p className="text-body-03 text-emphasis-medium">초대 코드를 공유해보세요.</p>
